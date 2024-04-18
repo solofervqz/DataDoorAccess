087f15e631d849853f9e5173d461318163b106b1
cristina <csuarezf09@gmail.com>
2024-04-18 11:39:12 -0600

    Update database

--- src/biblioteca/database.java
+++ src/biblioteca/database.java
@@ -4,6 +4,33 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class database {
+
+    // Método para conectar a la base de datos
+    public static Connection connectDb() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/bibliotec", "root", "");
+            return connect;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    // Método main para probar la funcionalidad
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*package biblioteca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class database {
         public static Connection connectDb(){
         
         try{
@@ -21,3 +48,4 @@ public class database {
     }
 
 }
+*/
