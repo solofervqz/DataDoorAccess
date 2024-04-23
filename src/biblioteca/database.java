db9736ce30e7f59878a7100d07c031dfd03a87cc
cristina <csuarezf09@gmail.com>
2024-04-23 12:41:31 -0600

    Delete some comments

--- src/biblioteca/database.java
+++ src/biblioteca/database.java
@@ -23,29 +23,3 @@ public class database {
 
     }
 }
-
-
-/*package biblioteca;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-public class database {
-        public static Connection connectDb(){
-        
-        try{
-        
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            Connection connect = 
-                DriverManager.getConnection("jdbc:mysql://localhost/bibliotec", "root", "");
-            
-            return connect;
-            
-        }catch(Exception e){e.printStackTrace();} 
-        
-        return null;
-    }
-
-}
-*/
